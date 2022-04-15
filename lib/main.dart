16c6cf2b1ebaa96915c479bc706ab1284de64d71
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2022-04-15 12:17:31 +0545

    git init

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Describes the part of the user interface represented by this widget.
+    return const MaterialApp(
+      home: Text("Hello World"),
+    );
+  }
+}
