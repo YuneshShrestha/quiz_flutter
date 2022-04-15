bf8963087216e008b477727b86e82a27f2fca78b
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2022-04-15 22:17:01 +0545

    push

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,40 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // Describes the part of the user interface represented by this widget.
     return const MaterialApp(
-      home: Text("Hello World"),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  var _questionNumber = 0;
+  final List<String> _question = ['Who are you?', 'What\'s your name'];
+  void _pressed() {
+    setState(() {
+      _questionNumber += 1;
+      // print('Hello ${_questionNumber.toString()}');
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Question"),
+      ),
+      body: Column(
+        children: [
+          Text(_question[_questionNumber].toString()),
+          RaisedButton(child: Text('Press'), onPressed: _pressed)
+        ],
+      ),
     );
   }
 }
